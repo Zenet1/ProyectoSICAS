01fa1e5f5b5707d78d6bcd5447f835c6a598cae7
Betoniix <miguelhumberto522@gmail.com>
2021-12-15 19:54:28 -0600

    Primer intento qr por correo

--- DB_PHP/Qr.Service.php
+++ DB_PHP/Qr.Service.php
@@ -1,9 +1,9 @@
 <?php
     use chillerlan\QRCode\QRCode;
     use chillerlan\QRCode\QROptions;
-    
     require 'vendor/autoload.php';
-    
+    require('Email.Service.php');
+
     $options = new QROptions(
       [
         'eccLevel' => QRCode::ECC_L,
@@ -13,4 +13,7 @@
     );
     
     $qrcode = (new QRCode($options))->render('pichula coqueta');
+    
+    $correo = new CorreoManejador();
+    $correo->EnviarCorreo("eduardzenet@outlook.com", "doexeo", "<img src='<?= $qrcode ?>' alt='QR Code' width='800' height='800'>" , $qrcode);
 ?>
