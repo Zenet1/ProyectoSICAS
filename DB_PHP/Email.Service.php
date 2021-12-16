761946d2597295aef3f1c402f8fa11cb4e60d153
Betoniix <miguelhumberto522@gmail.com>
2021-12-15 19:24:06 -0600

    Creacion de un objeto Enviador de correos

--- DB_PHP/Email.Service.php
+++ DB_PHP/Email.Service.php
@@ -1,41 +1,58 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
-    require 'Utileria.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'Utileria.php';
 
-//Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+class CorreoManejador
+{
+    private $mail;
+    private $isArchivo;
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = $_ENV['EMAILACCOUNT'];                     //SMTP username
-    $mail->Password   = $_ENV['EMAILACCOUNTPASSWORD'];                             //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = $_ENV['EMAILPORT'];                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    public function __construct() {
+        $this->isArchivo = false;
+        $this->mail = new PHPMailer(true);
 
-    //Recipients
-    $mail->setFrom($_ENV['EMAILACCOUNT'], 'Lince');
-    $mail->addAddress('eduardzenet@outlook.com');     //Add a recipient
+        try {
+            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $this->mail->isSMTP();                                            //Send using SMTP
+            $this->mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $this->mail->SMTPAuth = true;                                   //Enable SMTP authentication
+            $this->mail->Username = $_ENV['EMAILACCOUNT'];                     //SMTP username
+            $this->mail->Password = $_ENV['EMAILACCOUNTPASSWORD'];                             //SMTP password
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $this->mail->Port = $_ENV['EMAILPORT'];                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        } catch (Exception $e) {
+            echo "Error al configurar el EMAIl" . $e->getMessage();
+        }
+    }
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    public function EnviarCorreo($destinatario, String $asunto,String $mensaje, $archvio = NULL){
+        try {
+            $this->mail->setFrom($_ENV['EMAILACCOUNT'], 'SICAS');
+            $this->mail->addAddress('eduardzenet@outlook.com');     //Add a recipient
+            
+            if($this->isArchivo){
+                $this->mail->addAttachment($archvio);
+            }
+            
+            //Content
+            $this->mail->isHTML(true);                                  //Set email format to HTML
+            $this->mail->Subject = $asunto;
+            $this->mail->Body = $mensaje;
+            //$this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Doxeado';
-    $mail->Body    = 'no se que hago <b> con mi pichula in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $this->mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Algun error a ocurrido al enviar el mensaje";
+        }
+    }
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    public function setArchivo(bool $booleano){
+        $this->isArchivo = $booleano;
+    }
 }
 
+?>
