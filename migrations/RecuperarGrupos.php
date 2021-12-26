7c30804db72e79230998802936187a3e7a269949
Zenet1 <eduardo-orton2011@hotmail.com>
2021-12-26 01:45:07 -0600

    Corrección de error con respecto a la recuperación de Grupos
    No se consideraba que la asignatura no fuese identificada únicamente por la ClaveAsignatura, se debía considera su identificación por ClaveAsignatura e IDPlanEstudio

--- migrations/RecuperarGrupos.php
+++ migrations/RecuperarGrupos.php
@@ -7,11 +7,18 @@
     $archivo = file("bd_src/Licenciatura/AlumnosCargaDeAsignaturas.txt");
     $saltado = false;
 
+    //Querys
     $sqlInsert = "INSERT INTO sicas_test.grupos (IDAsignatura, IDProfesor, ClaveGrupo, Grupo) VALUES (?,?,?,?)";
-    $sqlrecuperarCasig = "SELECT IDAsignatura FROM asignaturas WHERE ClaveAsignatura=?";
+
+    $sqlrecuperarIDPlanAsig = "SELECT IDPlanEstudio FROM planesdeestudio WHERE ClavePlan=? AND VersionPlan=?";
+    $sqlrecuperarCasig = "SELECT IDAsignatura FROM asignaturas WHERE ClaveAsignatura=? AND IDPlanEstudio = ?";
+    
     $sqlrecuperarIprof = "SELECT IDProfesor FROM academicos WHERE ClaveProfesor=?"; 
 
+    //Objetos de recuperación
+    $obj_recuperarIDPlanAsig = $DB_CONEXION->prepare($sqlrecuperarIDPlanAsig);
     $obj_recuperarAsig = $DB_CONEXION->prepare($sqlrecuperarCasig);
+
     $obj_recuperarIprof = $DB_CONEXION->prepare($sqlrecuperarIprof);
     $obj_insert = $DB_CONEXION->prepare($sqlInsert);
 
@@ -25,19 +32,19 @@
 
         $data = explode("|", $linea);
 
-        $obj_recuperarAsig->execute(array($data[3]));
+        $obj_recuperarIDPlanAsig->execute(array($data[1], $data[2]));
+        $IDPlan = $obj_recuperarIDPlanAsig->fetch(PDO::FETCH_ASSOC);
+        $obj_recuperarAsig->execute(array($data[3], $IDPlan["IDPlanEstudio"]));
+
         $obj_recuperarIprof->execute(array($data[4]));
 
         $IDasig = $obj_recuperarAsig->fetch(PDO::FETCH_ASSOC);
         $IDprof = $obj_recuperarIprof->fetch(PDO::FETCH_ASSOC);
 
-        if(!isset($grupos[$data[3] . $data[4] . $data[5]])){
+        if(!isset($grupos[$data[1] . $data[2] . $data[3] . $data[4] . $data[5]])){
             $obj_insert->execute(array($IDasig["IDAsignatura"], $IDprof["IDProfesor"], $data[5], $data[6]));
-            $grupos[$data[3] . $data[4] . $data[5]] = $data[3] . $data[4] . $data[5];
+            $grupos[$data[1] . $data[2] . $data[3] . $data[4] . $data[5]] = $data[1] . $data[2] . $data[3] . $data[4] . $data[5];
         }
     }
     print_r($grupos);
-    function esValido($datos) : bool{
-        return $datos != null && $datos != false;
-    }
 ?>
--- migrations/RecuperarHorarios.php
+++ migrations/RecuperarHorarios.php
@@ -7,16 +7,26 @@
     $archivo = file("bd_src/Licenciatura/HorariosSesionesGrupo_Licenciatura.txt");
     $saltado = false;
 
+    //Querys
     $sqlInsert = "INSERT INTO sicas_test.horarios (IDGrupo, Dia, HoraInicioHorario, HoraFinHorario, IDSalon) VALUES (?, ?, ?, ?, ?);";
     $sqlrecuperarIDProfesor = "SELECT IDProfesor FROM academicos WHERE ClaveProfesor=?";
-    $sqlrecuperarIDAsignatura = "SELECT IDAsignatura FROM asignaturas WHERE ClaveAsignatura=?";
+
+    $sqlrecuperarIDPlanAsig = "SELECT IDPlanEstudio FROM planesdeestudio WHERE ClavePlan=? AND VersionPlan=?";
+    $sqlrecuperarIDAsignatura = "SELECT IDAsignatura FROM asignaturas WHERE ClaveAsignatura=? AND IDPlanEstudio=?";
+
     $sqlrecuperarIDGrupo = "SELECT IDGrupo FROM grupos WHERE ClaveGrupo=? AND IDProfesor=? AND IDAsignatura=?";
+    
     $sqlrecuperarEdificio = "SELECT IDEdificio FROM edificios WHERE NombreEdificio=?";
     $sqlrecuperarSalon = "SELECT IDSalon FROM salones WHERE NombreSalon=? AND IDEdificio=?";
 
+    //Objetos de recuperación
     $obj_recuperarIDProfesor = $DB_CONEXION->prepare($sqlrecuperarIDProfesor);
+
+    $obj_recuperarIDPlanAsig = $DB_CONEXION->prepare($sqlrecuperarIDPlanAsig);
     $obj_recuperarIDAsignatura = $DB_CONEXION->prepare($sqlrecuperarIDAsignatura);
+    
     $obj_recuperarIDGrupo = $DB_CONEXION->prepare($sqlrecuperarIDGrupo);
+    
     $obj_recuperarEdificio = $DB_CONEXION->prepare($sqlrecuperarEdificio);
     $obj_recuperarSalon = $DB_CONEXION->prepare($sqlrecuperarSalon);
 
@@ -38,7 +48,11 @@
         if(isset($IDEdificio["IDEdificio"])){
             $obj_recuperarIDProfesor->execute(array($data[3]));
             $IDProfesor = $obj_recuperarIDProfesor->fetch(PDO::FETCH_ASSOC);
-            $obj_recuperarIDAsignatura->execute(array($data[2]));
+            
+            $obj_recuperarIDPlanAsig->execute(array($data[0], $data[1]));
+            $IDPlanAsignatura = $obj_recuperarIDPlanAsig->fetch(PDO::FETCH_ASSOC);
+            $obj_recuperarIDAsignatura->execute(array($data[2], $IDPlanAsignatura["IDPlanEstudio"]));
+
             $IDAsignatura = $obj_recuperarIDAsignatura->fetch(PDO::FETCH_ASSOC);
             $obj_recuperarIDGrupo->execute(array($data[4], $IDProfesor["IDProfesor"], $IDAsignatura["IDAsignatura"]));
 
@@ -46,11 +60,9 @@
             $IDGrupo = $obj_recuperarIDGrupo->fetch(PDO::FETCH_ASSOC);
             $IDSalon = $obj_recuperarSalon->fetch(PDO::FETCH_ASSOC);
 
-            if(isset($IDSalon["IDSalon"])){
+            if(isset($IDSalon["IDSalon"]) && isset($IDGrupo["IDGrupo"])){
                 $obj_insert->execute(array($IDGrupo["IDGrupo"], $data[6], $data[7], $data[8], $IDSalon["IDSalon"]));
             }
-        }else{
-            continue;
         }
     }
 ?>
