a41ffda2acc387c23fe8981ea43ef5ce84c4c0b5
Betoniix <miguelhumberto522@gmail.com>
2021-12-31 16:57:46 -0600

    admin

--- src/app/services/administrador/administrador.service.ts
+++ src/app/services/administrador/administrador.service.ts
@@ -8,7 +8,7 @@ import { FormGroup } from '@angular/forms';
 export class AdministradorService {
   API_GestionBD:string = "/ProyectoSICAS/DB_PHP/BDControl.Service.php";
   API_Alerta:string = "/ProyectoSICAS/DB_PHP/Alerta.Service.php";
-  API_Email:string = '';
+  API_Email:string = '/ProyectoSICAS/DB_PHP/Email.Service.php';
   API_ObtenerEdificios:string = "/ProyectoSICAS/DB_PHP/DevolverEdificios.Service.php";
   API_ObtenerOficinas:string = "/ProyectoSICAS/DB_PHP/DevolverOficinas.Service.php";
   API_RegistrarOficina:string = "/ProyectoSICAS/DB_PHP/RegistrarOficina.Service.php";
