c8a29a52d217f3eb638ff1e4ab0127f622e97565
Betoniix <miguelhumberto522@gmail.com>
2021-12-31 22:28:19 -0600

    SICEI

--- DB_PHP/SICEI.Service.php
+++ DB_PHP/SICEI.Service.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-
 $numArchivos = intval($_POST["numArchivos"]);
 
 for ($i = 0; $i < $numArchivos; $i++) {
