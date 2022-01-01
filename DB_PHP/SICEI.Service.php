62658a0fd7a995362ed404121aeaf2b1b48712aa
Betoniix <miguelhumberto522@gmail.com>
2021-12-31 22:20:42 -0600

    Sistema de validacion

--- DB_PHP/SICEI.Service.php
+++ DB_PHP/SICEI.Service.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 $numArchivos = intval($_POST["numArchivos"]);
+
 for ($i = 0; $i < $numArchivos; $i++) {
-    print_r($_FILES["archivo" . $i]);
+    move_uploaded_file($_FILES["archivo" . $i]["tmp_name"], "docs/" . $_FILES["archivo" . $i]["name"]);
 }
