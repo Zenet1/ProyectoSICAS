ab36ea2b714715d1965ddbfa51fa6cea34f6a9c3
Zenet1 <eduardo-orton2011@hotmail.com>
2022-01-02 00:39:24 -0600

    Creación de la validación de oficinas duplicadas

--- DB_PHP/RegistrarOficina.Service.php
+++ DB_PHP/RegistrarOficina.Service.php
@@ -5,21 +5,45 @@
     include "BD_Conexion.php";
     $json = file_get_contents('php://input');
     $datos = json_decode($json);
-
+    
     insertarOficina((array)$datos, $DB_CONEXION);
 
     function insertarOficina(array $oficina, PDO $Conexion) : void{
         $sql_insertarOficina = "INSERT INTO oficinas (NombreOficina, Departamento, IDEdificio) VALUES (?, ?, ?)";
+
         $sql_recuperarIDEdificio = "SELECT IDEdificio FROM edificios WHERE NombreEdificio = ?";
 
         $obj_recuperarIDEdificio = $Conexion->prepare($sql_recuperarIDEdificio);
         $obj_insertarOficina = $Conexion->prepare($sql_insertarOficina);
 
         $obj_recuperarIDEdificio->execute(array($oficina["edificio"]));
-        $IDEdificio = $obj_recuperarIDEdificio->fetchAll(PDO::FETCH_ASSOC);
-        if(isset($IDEdificio[0]["IDEdificio"])){
-            echo "Se ha registrado la oficina satisfactoriamente";
-            $obj_insertarOficina->execute(array($oficina["oficina"], $oficina["departamento"], $IDEdificio[0]["IDEdificio"]));
+        $IDEdificio = $obj_recuperarIDEdificio->fetch(PDO::FETCH_ASSOC);
+        if(!validarOficinas($oficina, $IDEdificio, $Conexion)){
+            if(isset($IDEdificio[0]["IDEdificio"])){
+                $obj_insertarOficina->execute(array($oficina["oficina"], $oficina["departamento"], $IDEdificio["IDEdificio"]));
+            }
+        }else{
+            echo "error";
         }
+    }
+
+    function validarOficinas(array $oficina, array $IDEdificio, PDO $Conexion): bool
+    {
+        $existe = false;
+        
+        $sql_validarOficinasDuplicadas = "SELECT NombreOficina, Departamento, IDEdificio WHERE NombreOficina=? AND Departamento=? AND IDEdificio=?";
+
+        $obj_validarOficinasDuplicadas = $Conexion->prepare($sql_validarOficinasDuplicadas);
+
+        $obj_validarOficinasDuplicadas->execute(array($oficina["oficina"], $oficina["departamento"], $IDEdificio["IDEdificio"]));
+
+        $oficinaDuplicada = $obj_validarOficinasDuplicadas->fetch(PDO::FETCH_ASSOC);
+
+        if(isset($oficinaDuplicada)){
+            $existe = true;
+        }else{
+            $existe = false;
+        }
+        return $existe;
     }
 ?>
