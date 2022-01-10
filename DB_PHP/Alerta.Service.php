1f13e603e6b9879433d815e96e9aa65db59da4e9
Zenet1 <eduardo-orton2011@hotmail.com>
2022-01-09 19:52:36 -0600

    Encapsulamiento de operaciones relacionadas a Obtener fechas de Asistencia en una función en Alerta.Service

--- DB_PHP/Alerta.Service.php
+++ DB_PHP/Alerta.Service.php
@@ -2,58 +2,92 @@
 include "BD_Conexion.php";
 
 $json = file_get_contents('php://input');
-$datos = (array)json_decode($json);
+$datos_entrada = (array)json_decode($json);
 
-$arraygruposID = array();
-$arrayClaveGrupo = array();
-$alumnos = array();
-$Profesores = array();
-$usuariosImplicados = array();
-$datos_enviar = array();
+alertarAfectados($datos_entrada, $DB_CONEXION);
 
-$obj_obtenerFechasAsistencia = $DB_CONEXION->prepare("SELECT ASIS.FechaAl,ALM.IDAlumno FROM asistenciasalumnos AS ASIS INNER JOIN alumnos AS ALM ON ALM.IDAlumno=ASIS.IDAlumno WHERE ASIS.FechaAl >= ? AND ASIS.FechaAl <= ? AND ALM.Matricula = ?");
-$obj_obtenerFechasAsistencia->execute(array($datos["fechaInicio"], $datos["fechaFin"], $datos["matricula"]));
+function alertarAfectados(array $datos_entrada, PDO $Conexion){
 
-$arrayFechasAsistidas = $obj_obtenerFechasAsistencia->fetchAll(PDO::FETCH_ASSOC);
+    $arraygruposID = array();
+    $arrayClaveGrupo = array();
+    $alumnos = array();
+    $Profesores = array();
+    $usuariosImplicados = array();
+    $datos_enviar = array();
+    $arrayFechasAsistidas = obtenerFechasAsistencia($datos_entrada["fechaInicio"], $datos_entrada["fechaFin"], $datos_entrada["matricula"], $Conexion);
 
-$obj_obtenerGruposAsistidos = $DB_CONEXION->prepare("SELECT CGAC.IDGrupo,GPS.ClaveGrupo,GPS.Grupo FROM reservacionesalumnos AS RSAL INNER JOIN cargaacademica AS CGAC ON CGAC.IDCarga=RSAL.IDCarga INNER JOIN grupos AS GPS ON GPS.IDGrupo=CGAC.IDGrupo WHERE RSAL.FechaReservaAl = ? AND CGAC.IDAlumno = ?");
+    $sql_obtenerGruposAsistidos = "SELECT CGAC.IDGrupo, GPS.ClaveGrupo, GPS.Grupo FROM reservacionesalumnos AS RSAL 
+    INNER JOIN cargaacademica AS CGAC ON CGAC.IDCarga = RSAL.IDCarga 
+    INNER JOIN grupos AS GPS ON GPS.IDGrupo = CGAC.IDGrupo 
+    WHERE RSAL.FechaReservaAl = ? AND CGAC.IDAlumno = ?";
 
-$obj_obtenerAlumnosAfectados = $DB_CONEXION->prepare("SELECT ALM.NombreAlumno,ALM.ApellidoPaternoAlumno,ALM.ApellidoMaternoAlumno,ALM.CorreoAlumno FROM alumnos AS ALM INNER JOIN cargaacademica AS CGAC ON ALM.IDAlumno = CGAC.IDAlumno WHERE CGAC.IDGrupo = ? AND ALM.Matricula != ?");
+    $obj_obtenerGruposAsistidos = $Conexion->prepare($sql_obtenerGruposAsistidos);
 
-$obj_obtenerProfesores = $DB_CONEXION->prepare("SELECT ACAD.CorreoProfesor,ACAD.NombreProfesor,ACAD.ApellidoPaternoProfesor, ACAD.ApellidoMaternoProfesor FROM academicos AS ACAD INNER JOIN grupos AS GPS ON GPS.IDProfesor=ACAD.IDProfesor WHERE GPS.IDGrupo=?");
+    $sql_obtenerAlumnosAfectados = "SELECT ALM.NombreAlumno, ALM.ApellidoPaternoAlumno, ALM.ApellidoMaternoAlumno, ALM.CorreoAlumno FROM alumnos AS ALM 
+    INNER JOIN cargaacademica AS CGAC ON ALM.IDAlumno = CGAC.IDAlumno 
+    WHERE CGAC.IDGrupo = ? AND ALM.Matricula != ?";
 
-foreach ($arrayFechasAsistidas as $fechaAsistida) {
-    $obj_obtenerGruposAsistidos->execute(array($fechaAsistida["FechaAl"], $fechaAsistida["IDAlumno"]));
-    $grupos = $obj_obtenerGruposAsistidos->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($grupos as $grupo) {
-        if (!isset($arraygruposID[$grupo["IDGrupo"]])) {
-            $arraygruposID[$grupo["IDGrupo"]] = $grupo["IDGrupo"];
-            $arrayClaveGrupo[] = array("ClaveGrupo" => $grupo["ClaveGrupo"], "Grupo" => trim($grupo["Grupo"]));
+    $obj_obtenerAlumnosAfectados = $Conexion->prepare($sql_obtenerAlumnosAfectados);
+
+    $sql_obtenerProfesores = "SELECT ACAD.CorreoProfesor, ACAD.NombreProfesor, ACAD.ApellidoPaternoProfesor, ACAD.ApellidoMaternoProfesor FROM academicos AS ACAD 
+    INNER JOIN grupos AS GPS ON GPS.IDProfesor = ACAD.IDProfesor 
+    WHERE GPS.IDGrupo = ?";
+
+    $obj_obtenerProfesores = $Conexion->prepare($sql_obtenerProfesores);
+
+    foreach ($arrayFechasAsistidas as $fechaAsistida) {
+
+        $obj_obtenerGruposAsistidos->execute(array($fechaAsistida["FechaAl"], $fechaAsistida["IDAlumno"]));
+        $grupos = $obj_obtenerGruposAsistidos->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($grupos as $grupo) {
+            if (!isset($arraygruposID[$grupo["IDGrupo"]])) {
+                $arraygruposID[$grupo["IDGrupo"]] = $grupo["IDGrupo"];
+                $arrayClaveGrupo[] = array("ClaveGrupo" => $grupo["ClaveGrupo"], "Grupo" => trim($grupo["Grupo"]));
+            }
         }
     }
-}
 
-foreach ($arraygruposID as $clave => $valor) {
-    $obj_obtenerAlumnosAfectados->execute(array($valor, $datos["matricula"]));
-    $obj_obtenerProfesores->execute(array($valor));
-    $datosProfesor = $obj_obtenerProfesores->fetch(PDO::FETCH_ASSOC);
-    $datosAlumnos = $obj_obtenerAlumnosAfectados->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($arraygruposID as $clave => $valor) {
 
-    if (!isset($Profesores[trim($datosProfesor["CorreoProfesor"])])) {
-        $Profesores[trim($datosProfesor["CorreoProfesor"])] = "";
-        $usuariosImplicados[] = array(trim($datosProfesor["CorreoProfesor"]) => $datosProfesor["NombreProfesor"] . " " . $datosProfesor["ApellidoPaternoProfesor"] . " " . $datosProfesor["ApellidoMaternoProfesor"]);
-    }
+        $obj_obtenerAlumnosAfectados->execute(array($valor, $datos_entrada["matricula"]));
+        $obj_obtenerProfesores->execute(array($valor));
+        $datosProfesor = $obj_obtenerProfesores->fetch(PDO::FETCH_ASSOC);
+        $datosAlumnos = $obj_obtenerAlumnosAfectados->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($datosAlumnos as $alumno) {
-        if (!isset($alumnos[trim($alumno["CorreoAlumno"])])) {
-            $alumnos[trim($alumno["CorreoAlumno"])] = "";
-            $usuariosImplicados[] = array(trim($alumno["CorreoAlumno"]) => $alumno["NombreAlumno"] . " " . $alumno["ApellidoPaternoAlumno"] . " " . $alumno["ApellidoMaternoAlumno"]);
+        if (!isset($Profesores[trim($datosProfesor["CorreoProfesor"])])) {
+            $Profesores[trim($datosProfesor["CorreoProfesor"])] = "";
+            $usuariosImplicados[] = array(trim($datosProfesor["CorreoProfesor"]) => $datosProfesor["NombreProfesor"] . " " . $datosProfesor["ApellidoPaternoProfesor"] . " " . $datosProfesor["ApellidoMaternoProfesor"]);
+        }
+
+        foreach ($datosAlumnos as $alumno) {
+
+            if (!isset($alumnos[trim($alumno["CorreoAlumno"])])) {
+                $alumnos[trim($alumno["CorreoAlumno"])] = "";
+                $usuariosImplicados[] = array(trim($alumno["CorreoAlumno"]) => $alumno["NombreAlumno"] . " " . $alumno["ApellidoPaternoAlumno"] . " " . $alumno["ApellidoMaternoAlumno"]);
+            }
         }
     }
+
+    $datos_enviar["usuarios"] = $usuariosImplicados;
+    $datos_enviar["grupos"] = $arrayClaveGrupo;
+
+    echo json_encode($datos_enviar);
+    flush();
 }
 
-$datos_enviar["usuarios"] = $usuariosImplicados;
-$datos_enviar["grupos"] = $arrayClaveGrupo;
+function obtenerFechasAsistencia(string $fechaInicio, string $fechaFin, string $matricula, PDO $Conexion) : array{
 
-echo json_encode($datos_enviar);
-flush();
+    $sql_obtenerFechasAsistencia = "SELECT ASIS.FechaAl, ALM.IDAlumno FROM asistenciasalumnos AS ASIS 
+    INNER JOIN alumnos AS ALM ON ALM.IDAlumno = ASIS.IDAlumno 
+    WHERE ASIS.FechaAl >= ? AND ASIS.FechaAl <= ? AND ALM.Matricula = ?";
+    
+    $obj_obtenerFechasAsistencia = $Conexion->prepare($sql_obtenerFechasAsistencia);
+
+    $obj_obtenerFechasAsistencia->execute(array($fechaInicio, $fechaFin, $matricula));
+
+    $arrayFechasAsistidas = $obj_obtenerFechasAsistencia->fetchAll(PDO::FETCH_ASSOC);
+
+    return $arrayFechasAsistidas;
+}
+
