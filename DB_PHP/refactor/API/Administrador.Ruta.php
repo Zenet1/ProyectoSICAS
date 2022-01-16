395f68679b8f24286e772ff1588602f6df0bde0a
Betoniix <miguelhumberto522@gmail.com>
2022-01-15 20:16:50 -0600

    cambios en las rutas

--- DB_PHP/refactor/API/Administrador.Ruta.php
+++ DB_PHP/refactor/API/Administrador.Ruta.php
@@ -20,6 +20,12 @@ include_once("../Clases/Query.Class.php");
 include_once("../Clases/Fechas.Class.php");
 include_once("../Clases/ArchivosControl.Class.php");
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+include_once("../Clases/Query.Class.php");
+include_once("Servicios/Usuarios/AutenticarAlumno.Servicio.php");
+include_once("Servicios/Usuarios/AutenticarTrabajador.Servicio.php");
+
 $json = file_get_contents('php://input');
 $datos = json_decode($json);
 
